4b7f7333a1dffc9140b6813d34004e9889e149c1
Paola Nascimento <132537333+paolaalim@users.noreply.github.com>
2025-08-12 15:13:19 -0300

    Update uuidGenerator.ts

--- src/tools/uuidGenerator.ts
+++ src/tools/uuidGenerator.ts
@@ -1,1 +1,20 @@
-uuid
+import { v4 as uuidv4 } from 'uuid';
+
+
+export interface GenerateUuidArgs {
+  count?: number;
+  format?: 'formatted' | 'raw';
+}
+
+// Gera um ou mais UUIDs 
+export function generateUuids(options: GenerateUuidArgs = {}): string[] {
+  const { count = 1, format = 'formatted' } = options;
+  const uuids: string[] = [];
+
+  for (let i = 0; i < count; i++) {
+    const newUuid = uuidv4();
+    uuids.push(format === 'raw' ? newUuid.replace(/-/g, '') : newUuid);
+  }
+
+  return uuids;
+}
