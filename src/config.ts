145698ac451084679f5ccee077c4de178a169e41
Dayane Viana <128438273+DaiyWang@users.noreply.github.com>
2025-08-14 14:36:25 -0300

    adicionei o import "z"

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,10 @@
+import { z } from 'zod'; 
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
-  // ALTERAÇÃO AQUI: Torna a CLAUDE_API_KEY opcional
+
   CLAUDE_API_KEY: z.string().optional().default(''),
+  
   CLAUDE_API_URL: z.string().url().default('https://api.anthropic.com/v1/messages'),
 });
 
